6c680f5eb74b8a5ba253deae3afee383e1b33552
sanronick <sanronick@yahoo.com.ar>
2023-05-16 17:27:42 -0300

    añadido js, footer y estilos

---
+++ js/footer.js
@@ -0,0 +1,17 @@
+let footer=`<div>
+<a href="https://wa.me/5491169950277"><img src="img/logowhatsapp.svg" class="ws_logo" alt="Logo de Whats app"></a>
+</div>
+<h3 class="tituloFooter">Visita nuestras redes</h3>
+<ul class="redes_footer">
+    <li><a href=""><img src="img/logofacebook.svg" class="logos" alt="Logo de Facebook"></a></li>
+    <li><a href=""><img src="img/logoemail.svg" class="logos" alt="Logo de Gmail"></a></li>
+    <li><a href=""><img src="./img/logoinstagram.svg" class="logos" alt="Logo de Instagram"></a></li>
+</ul> 
+<ul class="redes_footer">
+    <li class="condiciones-generales">Condiciones Generales</li>
+    <li class="politica-seguridad">Politica de Seguridad</li>
+    <li class="terminos-condiciones">Terminos y condiciones de uso</li>
+</ul>               
+<div id="animacion"><p id="derechosreservados">&#169 2023 grupo 4 Full Stack Python - All Rights Reserved</p></div>`
+
+document.getElementById('pie').innerHTML=footer
